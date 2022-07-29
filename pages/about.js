048f02ca10b81dc630e2cd5507a62bb69d9eeb7f
talhatelli <ahmettalhatelli1453@gmail.com>
2022-07-29 11:31:45 +0300

    token

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,20 @@
-import Layout from "./component/layout";
-import Photo from "./imgComponent/photo";
+import Layout from './component/layout';
+import Photo from './imgComponent/photo';
 function About() {
   return (
     <Layout>
       <Photo />
       <div className="box">
-        <h1>Öz Biyografi</h1>
-        <p>
-          Merhabalar <br />
-          Adım Ahmet Talha soyadım Telli 21 yaşındayım Üniversite 3. sınıfım.
-          2001 de Malatya Battalgazi Alişar körmolla köyünde doğdum. İlk okul
-          hayatımı körmolla ilk okulunda bitirdim .Orta okul sürecimi Şehit
-          Servet Aktaş orta okulu ve 91.000 Dev Öğrenci orta okulunda bitirdim.
-          Liseyi Sadrettin Konevi AİHL den bitirdim .Üniversite hayatımı da
-          Kahramanmaraş Sütçü İmam Üniversite sinde devam ediyorum.
-        </p>
+        <div className="box-text">
+          {' '}
+          <h1>Öz Biyografi</h1>
+          <p>
+            Merhabalar <br />
+            Adım Ahmet Talha soyadım Telli 21 yaşındayım Üniversite 3. sınıfım. 2001 de Malatya Battalgazi Alişar körmolla köyünde doğdum. İlk okul hayatımı körmolla ilk okulunda bitirdim .Orta okul
+            sürecimi Şehit Servet Aktaş orta okulu ve 91.000 Dev Öğrenci orta okulunda bitirdim. Liseyi Sadrettin Konevi AİHL den bitirdim .Üniversite hayatımı da Kahramanmaraş Sütçü İmam Üniversite
+            sinde devam ediyorum.
+          </p>
+        </div>
       </div>
     </Layout>
   );
